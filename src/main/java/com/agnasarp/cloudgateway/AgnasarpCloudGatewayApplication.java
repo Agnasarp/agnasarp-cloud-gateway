7fc99f646a5f5d54aabc798ddcf2c95b0ff99c9b
Agnasarp <info.agnasarp@gmail.com>
2021-03-30 13:48:52 +0530

    Removed unnecessary configurations.

--- src/main/java/com/agnasarp/cloudgateway/AgnasarpCloudGatewayApplication.java
+++ src/main/java/com/agnasarp/cloudgateway/AgnasarpCloudGatewayApplication.java
@@ -3,15 +3,13 @@ package com.agnasarp.cloudgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableHystrix
 public class AgnasarpCloudGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AgnasarpCloudGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AgnasarpCloudGatewayApplication.class, args);
+    }
 
 }
